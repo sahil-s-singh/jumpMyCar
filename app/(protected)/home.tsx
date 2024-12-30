0871a9d70d70cca1d012eba80bd815e329886683
sahil-s-singh <68877454+sahil-s-singh@users.noreply.github.com>
2024-12-30 15:53:52 -0600

    remove node modules

---
+++ app/(protected)/home.tsx
@@ -0,0 +1,29 @@
+// app/home.tsx
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title} className='text-sm'>Welcome to the Home Screen!</Text>
+      <Button title="Go to Login" onPress={() => router.push('/auth/login')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
